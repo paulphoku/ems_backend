d3587b4c139efba4a668c451c7cff7ef0f307179
paulphoku <paulphoku@gmail.com>
2020-04-25 17:58:00 +0200

    database hosted online

--- config/connection.js
+++ config/connection.js
@@ -3,17 +3,24 @@ const mysql = require('mysql');
 // const bodyParser = require('body-parser');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'ems_db'
+    //hosted db
+    host: 'bo1nmv2ew69eo0xxnbcs-mysql.services.clever-cloud.com',
+    user: 'us0p8xdgwfdp0hao',
+    password: '5ax8TJIiDoXJMLSz5qKq',
+    database: 'bo1nmv2ew69eo0xxnbcs'
+
+    //hosted db
+    // host: 'localhost',
+    // user: 'root',
+    // password: '',
+    // database: 'ems_db'
 });
 
 db.connect(function(error,res){
     if (!!error) {
         // console.log(error);
        res.json({
-           code : 100, 
+           code : 100,
            status : "Error in connection database: " + error
         });
 		// return;
@@ -21,7 +28,7 @@ db.connect(function(error,res){
         console.log('Connected to the database');
         // res.json({
         //     code : 200, 
-        //     status : "Connected to the database: "
+        //     status : "Connected to the database:"
         //  });
     }
 });
