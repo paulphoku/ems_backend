f041a27900c42e562f23d3330db791b8c4b7c9ea
paulphoku <paulphoku@gmail.com>
2020-09-01 13:46:22 +0200

    fixed lost connection error

--- config/connection.js
+++ config/connection.js
@@ -2,43 +2,87 @@ const mysql = require('mysql');
 // const express = require('express');
 // const bodyParser = require('body-parser');
 
-const db = mysql.createConnection({
-    //hosted db
+
+
+
+
+//- Connection configuration
+var db_config = {
     host: 'bo1nmv2ew69eo0xxnbcs-mysql.services.clever-cloud.com',
     user: 'us0p8xdgwfdp0hao',
     password: '5ax8TJIiDoXJMLSz5qKq',
     database: 'bo1nmv2ew69eo0xxnbcs'
+};
 
-    //hosted db
-    // host: 'localhost',
-    // user: 'root',
-    // password: '',
-    // database: 'ems_db'
+//- Create the connection variable
+var db = mysql.createConnection(db_config);
+
+//- Establish a new connection
+db.connect(function(err){
+    if(err) {
+        // mysqlErrorHandling(connection, err);
+        console.log("\n\t *** Cannot establish a connection with the database. ***");
+
+        db = reconnect(db);
+    }else {
+        console.log("\n\t *** New connection established with the database. ***")
+    }
 });
 
+//- Reconnection function
+function reconnect(db){
+    console.log("\n New connection tentative...");
 
-function handleDisconnect() {
-    // the old one cannot be reused.
+    //- Destroy the current connection variable
+    if(db) db.destroy();
 
-    db.connect(function (err) {              // The server is either down
-        if (err) {                                     // or restarting (takes a while sometimes).
-            console.log('error when connecting to db:', err);
-            setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-        }else{
-            console.log('Connected to database')
-        }                                    // to avoid a hot loop, and to allow our node script to
-    });                                     // process asynchronous requests in the meantime.
-    // If you're also serving http, display a 503 error.
-    db.on('error', function (err) {
-        console.log('db error', err);
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-            handleDisconnect();                         // lost due to either server restart, or a
-        } else {                                      // connnection idle timeout (the wait_timeout
-            throw err;                                  // server variable configures this)
+    //- Create a new one
+    var db = mysql.createConnection(db_config);
+
+    //- Try to reconnect
+    db.connect(function(err){
+        if(err) {
+            //- Try to connect every 2 seconds.
+            setTimeout(reconnect, 2000);
+        }else {
+            console.log("\n\t *** New connection established with the database. ***")
+            return db;
         }
     });
 }
 
-handleDisconnect();
+//- Error listener
+db.on('error', function(err) {
+
+    //- The server close the connection.
+    if(err.code === "PROTOCOL_CONNECTION_LOST"){    
+        console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
+        db = reconnect(db);
+    }
+
+    //- Connection in closing
+    else if(err.code === "PROTOCOL_ENQUEUE_AFTER_QUIT"){
+        console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
+        db = reconnect(db);
+    }
+
+    //- Fatal error : connection variable must be recreated
+    else if(err.code === "PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR"){
+        console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
+        db = reconnect(db);
+    }
+
+    //- Error because a connection is already being established
+    else if(err.code === "PROTOCOL_ENQUEUE_HANDSHAKE_TWICE"){
+        console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
+    }
+
+    //- Anything else
+    else{
+        console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
+        db = reconnect(db);
+    }
+
+});
 
 module.exports = db;
